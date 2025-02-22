82c3653e7066ba4e87ef02a593b094a9cd74056d
Florence Etienne <florence.etienne@mail.citytech.cuny.edu>
2025-02-22 01:13:30 -0500

    added movement to player

--- Project1/Scattered_Entropy/Assets/Scripts/Player.cs
+++ Project1/Scattered_Entropy/Assets/Scripts/Player.cs
@@ -8,13 +8,15 @@ public class Player : MonoBehaviour
     public float Speed = 3.0f;
     public Vector2 Velocity;
     public Rigidbody2D rigidBody2D;
-    //public Animator animator;
+
+    bool isWalking;
+    public Animator AnimatorWalkDown;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
-        //animator = GetComponent<Animator>();
+        AnimatorWalkDown = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,8 +27,24 @@ public class Player : MonoBehaviour
         Velocity = Vector2.zero;
         Velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
+        float SpeedNormalized = Velocity.magnitude;
+        if (SpeedNormalized > .2f) {
+            isWalking = true;
+        }
+
+        else {
+            isWalking = false;
+        }
+
         //normalizes the velocity's speed to -1 or 1
         Velocity.Normalize();
+        AnimatorWalkDown.SetBool("IsWalking", isWalking);
+
+        if (SpeedNormalized! > 0)
+        {
+            AnimatorWalkDown.SetFloat("Horizontal", Velocity.x);
+            AnimatorWalkDown.SetFloat("Vertical", Velocity.y);
+        }
         
         Velocity *= Speed;
         rigidBody2D.velocity = Velocity;
