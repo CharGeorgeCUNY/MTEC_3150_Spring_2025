61ebaba56c27660c1ef150dff2911b3a01eed34c
rakinola-citytech <rukayat.akinola@mail.citytech.cuny.edu>
2025-03-05 14:53:40 -0500

    assignmentOne

---
+++ 2DProject/Assets/Scripts/Platform.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    //platform controls
+    public Rigidbody2D rbPlatform;
+
+    //platform variables
+    public float platSpeed;
+
+    //player Controls
+    bool playerContact;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlatform = GetComponent<Rigidbody2D>();
+        playerContact = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(playerContact && (gameObject.tag == "fallingPlatform"))
+        {
+
+            Debug.Log("PlayerContact");
+            rbPlatform.velocity = new Vector2(0, -1 * platSpeed);
+        }
+        else if(playerContact && (gameObject.tag == "risingPlatform"))
+        {
+            Debug.Log("PlayerContact");
+            rbPlatform.velocity = new Vector2(0, 1 * platSpeed);
+        }
+        else
+        {
+            Debug.Log("NoPlayerContact");
+            rbPlatform.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //detect player
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerContact = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerContact = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("harzard"))
+        {
+            Debug.Log("KilledPlatform");
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerContact = true;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity += rbPlatform.velocity;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerContact = false;
+        }
+    }
+}
---
+++ 2DProject/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator animator;
+    SpriteRenderer sr;
+
+    public float speed = 3.0f;
+    public float jumpSpeed = 5.0f;
+
+    Vector2 velocity;
+    bool isMoving;
+
+    //note: could really use a jumping limitation 
+    bool canJump;
+    //bool groundCheck = true;
+    //int count = 0;
+
+    //game over
+    string currentSceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
+        currentSceneName = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = Vector2.zero;
+        velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //float NormalizedSpeed = velocity.magnitude;
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+
+        {
+            if (Input.GetKey(KeyCode.LeftArrow)){
+                sr.flipX = true;
+            }
+            else
+            {
+                sr.flipX = false;
+            }
+
+            isMoving = true;
+            animator.SetBool("isMoving", isMoving);
+        }
+        else
+        {
+            isMoving = false;
+            animator.SetBool("isMoving", isMoving);
+
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            canJump = true;
+            animator.SetBool("canJump", canJump);
+        }
+        else
+        {
+            canJump = false;
+            animator.SetBool("canJump", canJump);
+        }
+        velocity.Normalize();
+
+        velocity *= speed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if((collision.gameObject.tag == "harzard")||(collision.gameObject.tag == "enterRock"))
+        {
+            //gameObject.SetActive(false);
+            GameOver();
+        }
+
+
+    }
+    //playmovement Specific
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(velocity.x, rb.velocity.y);
+        //jumpScript
+        if (canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+    }
+
+    //note Scene resets when deathSpike,startRock, and endRock.
+    void GameOver()
+    {
+        SceneManager.LoadScene(currentSceneName);
+    }
+}
