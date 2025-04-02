81ff145d134c7198b2e4e1a7fb5728d2a4e346a8
osak830 <osak830@gmail.com>
2025-04-02 17:55:09 -0400

    Still not finished
    Added jump, speed limit, and more objects.
    Need to fix the camera still and add a way to finish the game

--- JKim_Fixed/Assets/Script/PlayMovement.cs
+++ JKim_Fixed/Assets/Script/PlayMovement.cs
@@ -9,6 +9,14 @@ public class PlayMovement : MonoBehaviour
 
     public float groundDrag;
 
+    public float jumpforce;
+    public float jumpcooldown;
+    public float airMulitplier;
+    bool readyToJump;
+
+    [Header("keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsround;
@@ -33,6 +41,7 @@ public class PlayMovement : MonoBehaviour
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsround);
 
         MyInput();
+        SpeedControl();
 
         // Handle Drag
         if (grounded)
@@ -48,12 +57,51 @@ public class PlayMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        //when to jump
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpcooldown);
+        }
     }
     private void MovePlayer()
     {
         // calculate movement direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        
-        rb.AddForce(moveDirection.normalized *moveSpeed * 10f, ForceMode.Force);
+
+
+        // on ground
+        if(grounded)
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // in air
+        else if(!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMulitplier, ForceMode.Force);
+    }
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+    private void Jump()
+    {
+        // reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpforce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
