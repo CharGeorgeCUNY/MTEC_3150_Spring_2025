e54eadce640aa236edfa41377709c1595cd0ce42
Sym Whysel <simone.whysel@gmail.com>
2025-04-02 23:04:06 -0400

    Squashed commit of the following:
    commit 6be3cd15548ccb6427ed8cd2bb5e2130372fc376
    Author: CharGeorgeCUNY <CHARLES.GEORGE55@citytech.cuny.edu>
    Date:   Tue Apr 1 22:03:11 2025 -0400
    
        Assignment3_Posted
    
    commit 1c9d877d80ec01512bc7d848fc6ca275df63e489
    Author: CharGeorgeCUNY <CHARLES.GEORGE55@citytech.cuny.edu>
    Date:   Wed Mar 26 17:16:11 2025 -0400
    
        DebugLecture
    
    commit 912c9515d384ea8e57d2d9d7c3583bf144cec8e1
    Author: hosoji <CHARLES.GEORGE55@citytech.cuny.edu>
    Date:   Wed Mar 19 21:24:55 2025 -0400
    
        Lighting lectureAftermath
    
    commit 992f2f41e46ea0b8e19ac7184a7e27a48ab6a6ff
    Author: CharGeorgeCUNY <CHARLES.GEORGE55@citytech.cuny.edu>
    Date:   Wed Mar 19 13:30:34 2025 -0400
    
        3D lighting lecture push
    
    commit 4f62c66191e26bd211c616fe16b9d50c508d0597
    Author: CharGeorgeCUNY <CHARLES.GEORGE55@citytech.cuny.edu>
    Date:   Wed Mar 19 13:30:08 2025 -0400
    
        Added Git LFS
    
    commit b995da8c2a6b4afba53a8ce89391c3c12bbabac5
    Author: hosoji <CHARLES.GEORGE55@citytech.cuny.edu>
    Date:   Wed Mar 12 20:47:04 2025 -0400
    
        Fixed FPS Controller
    
    commit 15bea9e9a263013357071baa2483cde4a358e7d2
    Author: hosoji <CHARLES.GEORGE55@citytech.cuny.edu>
    Date:   Wed Mar 12 19:34:47 2025 -0400
    
        Lecture4CinemachineAndController
    
    commit c51312cffd4eb34ad4cbd3f783a4b107bae11f10
    Author: hosoji <CHARLES.GEORGE55@citytech.cuny.edu>
    Date:   Thu Mar 6 21:37:41 2025 -0500
    
        Lecture 3 Camera type demos

---
+++ Debugging/Assets/Meteor.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : iDamageable
+{
+    // Start is called before the first frame update
+    private float StartTime;
+    [SerializeField]
+    private float DeathTime = 6f;
+    public float InitialForce=100f;
+    SpriteRenderer sr;
+
+    void Start()
+    {
+        StartTime = Time.realtimeSinceStartup;
+        Vector2 PositionVector = GameObject.FindObjectOfType<Ship>().transform.position - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(PositionVector.normalized * InitialForce);
+
+        //some edits
+        sr = gameObject.GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.realtimeSinceStartup - StartTime > DeathTime)
+        {
+            onDestroyTime();
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Ship>() != null)
+        {
+            collision.gameObject.GetComponent<Ship>().DoDamage();
+        }
+        
+    }
+
+
+    public override void DoDamage()
+    {
+        onDestroyDamage();
+    }
+
+    public void onDestroyDamage()
+    {
+        BlinkRed();
+        //Destroy(this.gameObject);
+    }
+    public void onDestroyTime()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public IEnumerator BlinkRed()
+    {
+        WaitForSeconds delay = new WaitForSeconds(0.05f);
+        gameamanger.GetGameManager().IncrementScore();
+        //canTakeDamage = false;
+        sr.color = Color.red;
+        yield return delay;
+        sr.color = Color.white;
+        yield return delay;
+        sr.color = Color.red;
+        yield return delay;
+        sr.color = Color.white;
+        yield return delay;
+        sr.color = Color.red;
+        yield return delay;
+        sr.color = Color.white;
+        yield return delay;
+        Destroy(this.gameObject);
+        //canTakeDamage = true;
+    }
+
+}
