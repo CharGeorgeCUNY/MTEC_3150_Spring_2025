02722acd7e26174f0f289fe010f08daf1b813e2a
Florence Etienne <florence.etienne@mail.citytech.cuny.edu>
2025-04-09 19:26:30 -0400

    added to capsule movement script

--- AutumnHunt/Assets/Script/CapsuleMover.cs
+++ AutumnHunt/Assets/Script/CapsuleMover.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CapsuleMover : MonoBehaviour
 {
+    Vector3 playerVelocity;
+    float playerJump;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class CapsuleMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 moveDist = Vector3.zero;
+        //walk
         
+        //jump
     }
 }
