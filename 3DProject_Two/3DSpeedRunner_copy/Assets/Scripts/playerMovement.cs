aa6a2861947719674127f1130c868239c69c04ff
rakinola-citytech <rukayat.akinola@mail.citytech.cuny.edu>
2025-04-17 20:11:57 -0400

    Project 3_Update to 3DSpeedRunner
    Moving cars and Small explosion, Bigger explosion felt disruptive and unnecessary.
    
    Smaller explosive also isn't much to my liking but happy to tryout the particle system

---
+++ 3DProject_Two/3DSpeedRunner_copy/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using UnityEngine.InputSystem;
+
+public class playerMovement : MonoBehaviour
+{
+
+    //playerControls
+    Rigidbody rb;
+    Vector3 move;
+
+    public float speed = 1.0f;
+
+    public Transform[] changeLane;
+    int arrayIndex = 1;
+
+    //jumpProgramming removed temporarily
+    //public LayerMask LayersToCheck;
+    //bool isGrounded = false;
+    //public float jumpHeight = 10.0f;
+
+    //damage and death
+    public Material colorChange;
+
+    bool canTakeDamage = true;
+    int life = 3;
+    public GameObject[] healthBar;
+
+    //hit Audio
+    AudioSource playerAudio;
+
+    //gameOver & sceneChangeSequence
+    public GameObject gameOverText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        move = Vector3.forward;
+        colorChange.color = Color.white;
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move = Vector3.forward;
+        rb.velocity = move * speed;
+
+        //jumpProgramming removed
+        //isGrounded = Physics.Raycast(transform.position, Vector3.down, GetComponent<CapsuleCollider>().height / 1.9f, LayersToCheck);
+        //Debug.Log("isGrounded = ");
+        //Debug.Log(isGrounded);
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //Debug.Log("RightArrow");
+            arrayIndex += 1;
+            arrayIndex = Mathf.Clamp(arrayIndex, 0, 2);
+            //Debug.Log(arrayIndex);
+
+            Vector3 moveLeft = new Vector3(changeLane[arrayIndex].position.x, 0, transform.position.z);
+            rb.MovePosition(moveLeft);
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //Debug.Log("LeftArrow");
+            arrayIndex -= 1;
+            arrayIndex = Mathf.Clamp(arrayIndex, 0, 2);
+            //Debug.Log(arrayIndex);
+
+
+            Vector3 moveRight = new Vector3(changeLane[arrayIndex].position.x, 0, transform.position.z);
+            rb.MovePosition(moveRight);
+        }
+
+        //jumpProgramming removed temporarily
+        //Debug.Log("Space = ");
+        //Debug.Log(Input.GetKeyDown(KeyCode.Space));
+
+        //if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        //{
+        //    rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);
+        //    //if(isGrounded && rb.velocity.y <= 0.0f)
+        //    //{
+        //    //    rb.velocity = new Vector3(rb.velocity.x,jumpHeight, rb.velocity.z);
+        //    //}
+        //}
+
+        if( life == 0 )
+        {
+            canTakeDamage = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.gameObject.tag == "hazard") && canTakeDamage)
+        {
+            //Debug.Log("hit hazard");
+            //Debug.Log(life);
+            playerAudio.Play();
+            life -= 1;
+            healthBar[life].SetActive(false);
+            StartCoroutine(BlinkRed());
+            
+            
+        }
+        else if(!canTakeDamage)
+        {
+            StartCoroutine(gameOver());
+        }
+
+        if(collision.gameObject.tag == "endLane")
+        {
+            changeScene();
+        }
+    }
+
+    public IEnumerator BlinkRed()
+    {
+        WaitForSeconds delay = new WaitForSeconds(0.05f);
+
+        colorChange.color = Color.red;
+        yield return delay;
+        colorChange.color = Color.white;
+        yield return delay;
+        colorChange.color = Color.red;
+        yield return delay;
+        colorChange.color = Color.white;
+        yield return delay;
+        colorChange.color = Color.red;
+        yield return delay;
+        colorChange.color = Color.white;
+        yield return delay;
+       
+    }
+
+    public IEnumerator gameOver()
+    {
+
+        WaitForSeconds delay = new WaitForSeconds(1.2f);
+        gameOverText.SetActive(true);
+        yield return delay;
+        changeScene();
+
+    }
+
+    void changeScene()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+
+}
+
---
+++ 3DProject_Two/3DSpeedRunner_copy/Assets/Scripts/sceneManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class sceneManager : MonoBehaviour
+{
+
+    public GameObject[] hazards;
+    int hazardChoice;
+    int laneChoice1;
+    int laneChoice2;
+
+    //spawning
+    Vector3 spawnPos;
+    Vector3 spawnPos1;
+    public float spawnOffset = 8.0f;
+
+    float currentTime = 0f;
+    float lastTime = 0f;
+    float timeInterval = 1.5f;
+
+    //playerMovement script referencing
+    public playerMovement player;
+    //audio
+    AudioSource sound;
+    public AudioClip warningSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 4.0f, timeInterval);
+        sound = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        hazardChoice = Random.Range(0, 2); //index: [0,1]
+
+        currentTime += 1;
+        currentTime = Mathf.Clamp(currentTime, 0, 4);
+        laneChoice1 = Random.Range(0, 3); //index: [0,1,2]
+        laneChoice2 = Random.Range(0, 3);
+
+    }
+
+    void Spawn()
+    {
+
+        spawnPos = new Vector3(player.changeLane[laneChoice1].position.x,player.transform.position.y, player.transform.position.z + spawnOffset);
+        StartCoroutine(audioPlayer());
+        Instantiate(hazards[hazardChoice], spawnPos, Quaternion.Euler(new Vector3(0, 180, 0)));
+        lastTime = currentTime;
+
+        if(currentTime - lastTime >= timeInterval)
+        {
+            spawnPos1 = new Vector3(player.changeLane[laneChoice2].position.x, player.transform.position.y, player.transform.position.z + spawnOffset);
+            Debug.Log("play warning");
+            StartCoroutine(audioPlayer());
+            Instantiate(hazards[hazardChoice], spawnPos1, Quaternion.Euler(new Vector3(0, 180, 0)));
+
+        }
+        else
+        {
+            currentTime = 0f;
+        }
+
+    }
+
+    public IEnumerator audioPlayer()
+    {
+
+        WaitForSeconds delay = new WaitForSeconds(1.0f);
+        sound.PlayOneShot(warningSound, 0.8f);
+        yield return delay;
+
+    }
+
+
+}
