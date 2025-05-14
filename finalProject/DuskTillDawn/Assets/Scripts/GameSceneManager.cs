77e214ea2f939ee05c91d30693ba693f9fa142c9
rakinola-citytech <rukayat.akinola@mail.citytech.cuny.edu>
2025-05-14 18:09:41 -0400

    PrefinalSubmissionUpdate
    missing Date-Night Cycle and InGameTimeTracker

--- finalProject/DuskTillDawn/Assets/Scripts/GameSceneManager.cs
+++ finalProject/DuskTillDawn/Assets/Scripts/GameSceneManager.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameSceneManager : MonoBehaviour
 {
     public Image fullSpeedBar;
     public playerMovement player;
-
 
     // Start is called before the first frame update
     void Start()
--- finalProject/DuskTillDawn/Assets/Scripts/playerMovement.cs
+++ finalProject/DuskTillDawn/Assets/Scripts/playerMovement.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerMovement : MonoBehaviour
 {
     Rigidbody rbPlayer;
 
     float speedDefault = 7.0f;
-    public float fullSpeedfill = 10.0f;
+    public float fullSpeedfill = 1.0f;
+
+    //checkPoints Signs
+    public GameObject checkPointOneText;
+    public GameObject checkPointTwoText;
+    public GameObject checkPointThreeText;
+    public GameObject theEndText;
+
+    //checkPoints GameObject Transform
+    public Transform checkPointOnePos;
+    public Transform checkPointTwoPos;
+    public Transform checkPointThreePos;
+
+    //reload scene, collision with startBorder
+    string currentSceneName;
 
     // Start is called before the first frame update
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody>();
+        currentSceneName = SceneManager.GetActiveScene().name;
     }
 
     // Update is called once per frame
@@ -20,6 +36,8 @@ public class playerMovement : MonoBehaviour
     {
 
         setSpeed();
+        Debug.Log("fullSpeedfill = " + fullSpeedfill);
+        Debug.Log("speedSetting = " + speedDefault);
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
@@ -34,7 +52,6 @@ public class playerMovement : MonoBehaviour
         {
             Quaternion LookingAt = Quaternion.LookRotation(transform.right, transform.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, LookingAt, -45.0f * Time.deltaTime);
-            //rbPlayer.velocity = -transform.right * 2.0f;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
@@ -45,46 +62,96 @@ public class playerMovement : MonoBehaviour
 
     void setSpeed()
     {
-        if (Input.GetKey(KeyCode.Z)){
-            speedDefault = 10.0f;
-            fullSpeedfill -= 1.0f * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.X))
-        {
+        if (Input.GetKey(KeyCode.Z) && (fullSpeedfill > 0)){
             speedDefault = 15.0f;
+        }
+        else
+        {
+            speedDefault = 7.0f;
+        }
+
+        if ((rbPlayer.velocity.z > 0.5f) && Input.GetKey(KeyCode.Z)){
+            fullSpeedfill -= 0.2f * Time.deltaTime;
         }
 
     }
 
-    //public IEnumerator roadBlock1()
-    //{
-    //    WaitForSeconds delay = new WaitForSeconds(5.0f);
-    //    blockOne.SetActive(true);
-    //    yield return delay;
-    //    blockOne.SetActive(false);
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "hazardOne")
+        {
+            transform.position = new Vector3(checkPointOnePos.position.x,checkPointOnePos.position.y,
+                checkPointOnePos.position.z - 10.0f);
+        }
+        if (collision.gameObject.tag == "hazardTwo")
+        {
+            transform.position = new Vector3(checkPointTwoPos.position.x, checkPointTwoPos.position.y,
+                checkPointTwoPos.position.z - 10.0f);
+        }
+        if (collision.gameObject.tag == "hazardThree")
+        {
+            transform.position = new Vector3(checkPointThreePos.position.x, checkPointThreePos.position.y,
+                checkPointThreePos.position.z - 10.0f);
+        }
 
-    //}
-    //public IEnumerator roadBlock2()
-    //{
-    //    WaitForSeconds delay = new WaitForSeconds(5.0f);
-    //    blockTwo.SetActive(true);
-    //    yield return delay;
-    //    blockTwo.SetActive(false);
+    }
 
-    //}
-    //public IEnumerator roadBlock3()
-    //{
-    //    WaitForSeconds delay = new WaitForSeconds(5.0f);
-    //    blockOne.SetActive(true);
-    //    yield return delay;
-    //    blockTwo.SetActive(false);
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "start")
+        {
+            SceneManager.LoadScene(currentSceneName);
+        }
+        if (collision.gameObject.tag == "end")
+        {
+            StartCoroutine(theEnd());
+        }
+        if (collision.gameObject.tag == "One")
+        {
+            StartCoroutine(checkPointOne());
+        }
+        if (collision.gameObject.tag == "Two")
+        {
+            StartCoroutine(checkPointTwo());
+        }
+        if (collision.gameObject.tag == "Three")
+        {
+            StartCoroutine(checkPointThree());
+        }
 
-    //}
-    //public IEnumerator roadBlock4()
-    //{
-    //    WaitForSeconds delay = new WaitForSeconds(5.0f);
-    //    blockOne.SetActive(true);
-    //    yield return delay;
-    //    blockTwo.SetActive(false);
-    //}
+    }
+
+    public IEnumerator theEnd()
+    {
+        WaitForSeconds delay = new WaitForSeconds(1.8f);
+        theEndText.SetActive(true);
+        yield return delay;
+        theEndText.SetActive(false);
+        yield return delay;
+        SceneManager.LoadScene(currentSceneName);
+    }
+    public IEnumerator checkPointOne()
+    {
+        WaitForSeconds delay = new WaitForSeconds(1.5f);
+        checkPointOneText.SetActive(true);
+        yield return delay;
+        checkPointOneText.SetActive(false);
+        yield return delay;
+    }
+    public IEnumerator checkPointTwo()
+    {
+        WaitForSeconds delay = new WaitForSeconds(1.5f);
+        checkPointTwoText.SetActive(true);
+        yield return delay;
+        checkPointTwoText.SetActive(false);
+        yield return delay;
+    }
+    public IEnumerator checkPointThree()
+    {
+        WaitForSeconds delay = new WaitForSeconds(1.5f);
+        checkPointThreeText.SetActive(true);
+        yield return delay;
+        checkPointThreeText.SetActive(false);
+        yield return delay;
+    }
 }
